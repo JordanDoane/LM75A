eb719e5fec50c021b4180c5c4a448ea93e178483
Jordan <jordan.doane0@gmail.com>
2020-04-03 23:18:15 -0400

    first commit

---
+++ LM75A.py
@@ -0,0 +1,21 @@
+#lm75a.py
+from smbus2 import SMBus
+import sys
+
+def lm75a(address):
+
+    if 1 < len(sys.argv):
+        address = int(sys.argv[1], 16)
+
+    try:
+        bus = SMBus(1)
+        raw = bus.read_word_data(address, 0)& 0xffff
+        raw = ((raw << 8) & 0xffff) + (raw >> 8)
+        temperature = (raw / 32.0) / 8.0
+
+        return temperature
+        
+    except Exception as e: print(e)
+    
+    finally:
+        bus.close()
